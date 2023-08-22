fbef74f885031cade096bc97a683ca0f8b6d6482
ARNAB <arnabpal2002@gmail.com>
2023-08-22 22:03:26 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,47 @@
+const AWS = require("aws-sdk");
+const s3 = new AWS.S3(); // Initialize S3 client
+const dynamodb = new AWS.DynamoDB.DocumentClient(); // Initialize DynamoDB client
+
+// Checkpoint 1: Check if the record counts match
+async function checkRecordCounts() {
+  const sourceCount = sourceData.length;
+  const migratedCount = migratedData.length;
+
+  if(sourceCount === migratedCount) {
+    console.log("Record counts match.");
+  } else {
+    console.log("Record counts do not match.");
+  }
+}
+
+// Checkpoint 2: Verify data integrity using checksums
+async function verifyDataIntegrity() {
+  const sourceChecksum = calculateChecksum(sourceData);
+  const migratedChecksum = calculateChecksum(migratedData);
+
+  if (sourceChecksum === migratedChecksum) {
+    console.log("Data integrity verified using checksums.");
+  } else {
+    console.log("Data integrity check failed.");
+  }
+}
+
+
+
+// Additional checkpoints and validations can be added as needed.
+
+// Helper function to calculate checksum
+function calculateChecksum(data) {
+    //implementation using hash code 
+}
+
+// Call the validation functions
+(async () => {
+  try {
+    await checkRecordCounts();
+    await verifyDataIntegrity();
+    await validateSampleData(3); // Validate 3 random records
+  } catch (error) {
+    console.error("Error during validation:", error);
+  }
+})();
